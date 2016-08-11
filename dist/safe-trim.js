311a49d2b611ca4ed2239c16541663f8fbdea07c
haozi <syntaxright@gmail.com>
2016-08-11 19:49:14 +0800

    add zero-width-space

--- dist/safe-trim.js
+++ dist/safe-trim.js
@@ -1,5 +1,5 @@
 /*!
- * safe-trim v1.0.1
+ * safe-trim v1.0.2
  * (c) 2016 Jerry
  * Released under the MIT License.
  */
@@ -11,22 +11,30 @@
 
   function safeTrim(string) {
     var SP = ' '; // common space
+    var TAB = '\t'; // tab
     var CR = '\r'; //  Carriage Return, Used as a new line character in Mac OS before X
     var LF = '\n'; //  Line Feed, Used as a new line character in Unix/Mac OS X
     var CR_LF = '\r\n'; // Used as a new line character in Windows
+    var ZERO_WIDTH_SPACE = '\v' + // \x0B VT 垂直制表符
+    '\f' + //  \x0C FF 换页符
+    '​‌‍‎‏\u000b\u2028\u2029﻿';
+    var OTHER_SPACE = '\t\f  ᠎             　';
 
-    var OTHER_SPACE = '\t\u000b\f  ᠎             　\u2028\u2029﻿';
-    var ALL_SPACE = SP + CR + LF + CR_LF + OTHER_SPACE;
+    var ALL_SPACE = SP + TAB + CR + LF + CR_LF + ZERO_WIDTH_SPACE + OTHER_SPACE;
 
     var leftReg = new RegExp('^[' + ALL_SPACE + ']+');
     var rightReg = new RegExp('[' + ALL_SPACE + ']+$');
-    var otherReg = new RegExp('[' + OTHER_SPACE + ']', 'g');
+    var zeroReg = new RegExp('[' + ZERO_WIDTH_SPACE + ']+', 'g');
+    var otherReg = new RegExp('[' + OTHER_SPACE + ']+', 'g');
 
     return (string + '').replace(leftReg, '') // trim left
     .replace(rightReg, '') // trim right
-    .replace(new RegExp(CR_LF, 'g'), LF) // CR_LF to LF
-    .replace(new RegExp(CR, 'g'), LF) // single CR to LF
-    .replace(otherReg, '');
+    .replace(new RegExp(TAB, 'g'), '') // TAB => '  ' 2space
+    .replace(new RegExp(CR_LF, 'g'), LF) // '\r\n' => '\n'
+    .replace(new RegExp(CR, 'g'), LF) // single \r => '\n'
+    .replace(zeroReg, '') // Zero-width-space => ''
+    .replace(otherReg, '') // other => ''
+    .trim();
   }
 
   return safeTrim;
--- test/test_spec.js
+++ test/test_spec.js
@@ -3,24 +3,30 @@
 const safeTrim = require('../dist/safe-trim.common.js')
 
 describe('safeTrim', () => {
-  it('trim \\r\\n\\t', () => {
+  it('trim 0', () => {
+    let str = 'a\u200Bb   c	\r\n\r\td e\u2003       f ᠎             　   	   g'
+    let ret = safeTrim(str)
+    expect(ret).toEqual('ab   c\n\ndef      g')
+  })
+  
+  it('trim 1', () => {
     let str = '  "a":1    a	\r\n\r\t  ᠎             　b   	   '
     let ret = safeTrim(str)
     expect(ret).toEqual('"a":1    a\n\nb')
   })
-
+  
   it('converted CR CR-LR into LR', () => {
     // safeTrim('\r\n') === ''
     expect(safeTrim('   a\r\n\r\nb  ')).toEqual('a\n\nb')
     expect(safeTrim(' \r\n\r\n ')).toEqual('')
-
+    
     expect(safeTrim('  a\r\rb ')).toEqual('a\n\nb')
     expect(safeTrim('  \r\r\r  ')).toEqual('')
-
+    
     expect(safeTrim(' a\r\r\nb ')).toEqual('a\n\nb')
     expect(safeTrim(' \r\r\n  ')).toEqual('')
   })
-
+  
   it('trim BOM', () => {
     let str = '﻿{"a":1}'
     let ret
@@ -31,7 +37,7 @@ describe('safeTrim', () => {
     }
     expect(ret).toEqual('error!!!')
   })
-
+  
   it('safe trim Bom', () => {
     let str = '﻿{"a":1}'
     let ret = JSON.parse(safeTrim(str))
@@ -43,29 +49,29 @@ describe('bad args', () => {
   it('{}', () => {
     expect(safeTrim({})).toEqual('[object Object]')
   })
-
+  
   it('[]', () => {
     expect(safeTrim([])).toEqual('')
   })
-
+  
   it('NaN', () => {
     expect(safeTrim(NaN)).toEqual('NaN')
   })
-
+  
   it('undefined', () => {
     expect(safeTrim(undefined)).toEqual('undefined')
   })
-
+  
   it('null', () => {
     expect(safeTrim(null)).toEqual('null')
   })
-
+  
   it('0', () => {
     expect(safeTrim(0)).toEqual('0')
   })
-
+  
   it('function', () => {
-    let ret = safeTrim(safeTrim(function(){})).replace(/\s+/, '')
+    let ret = safeTrim(safeTrim(function () {})).replace(/\s+/g, '')
     expect(ret).toEqual('function(){}')
   })
 })
