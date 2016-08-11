a18171b2451e39e2e7448f9bbaa71fcb30104ba1
浩知 <haozhi.lyj@alibaba-inc.com>
2016-08-11 16:32:20 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # safe-trim
-trim all unsafe space
+## install
+```
+npm install safe-trim
+```
+## use
+```
+import safeTrim from 'safe-trim'
+safeTrim('    a      b  ')
+```
+
+## remove Invisible spaces
+
+```
+let str = '  "a":1    a \r\n\r\t  ᠎             　b       '
+let ret = safeTrim(str)
+expect(ret).toEqual('"a":1    a\n\nb')
+```
+
+## converted CR CR-LR into LR
+```
+a\r\n\r\nb  => 'a\n\nb'
+a\r\rb => 'a\n\nb'
+a\r\r\nb => 'a\n\nb'
+```
+
+## remove BOM
+```
+JSON.parse('﻿{"a":1}') // ❗️Error because BOM
+
+JSON.parse(safeTrim('﻿{"a":1}')) // ✅
+```
---
+++ test/test_spec.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const safeTrim = require('../dist/safe-trim.common.js')
+
+describe('safeTrim', () => {
+  it('trim \\r\\n\\t', () => {
+    let str = '  "a":1    a	\r\n\r\t  ᠎             　b   	   '
+    let ret = safeTrim(str)
+    expect(ret).toEqual('"a":1    a\n\nb')
+  })
+
+  it('converted CR CR-LR into LR', () => {
+    // safeTrim('\r\n') === ''
+    expect(safeTrim('   a\r\n\r\nb  ')).toEqual('a\n\nb')
+    expect(safeTrim(' \r\n\r\n ')).toEqual('')
+
+    expect(safeTrim('  a\r\rb ')).toEqual('a\n\nb')
+    expect(safeTrim('  \r\r\r  ')).toEqual('')
+
+    expect(safeTrim(' a\r\r\nb ')).toEqual('a\n\nb')
+    expect(safeTrim(' \r\r\n  ')).toEqual('')
+  })
+
+  it('trim BOM', () => {
+    let str = '﻿{"a":1}'
+    let ret
+    try {
+      JSON.parse(str)
+    } catch (e) {
+      ret = 'error!!!'
+    }
+    expect(ret).toEqual('error!!!')
+  })
+
+  it('safe trim Bom', () => {
+    let str = '﻿{"a":1}'
+    let ret = JSON.parse(safeTrim(str))
+    expect(ret).toEqual({a: 1})
+  })
+})
+
+describe('bad args', () => {
+  it('{}', () => {
+    expect(safeTrim({})).toEqual('[object Object]')
+  })
+
+  it('[]', () => {
+    expect(safeTrim([])).toEqual('')
+  })
+
+  it('NaN', () => {
+    expect(safeTrim(NaN)).toEqual('NaN')
+  })
+
+  it('undefined', () => {
+    expect(safeTrim(undefined)).toEqual('undefined')
+  })
+
+  it('null', () => {
+    expect(safeTrim(null)).toEqual('null')
+  })
+
+  it('0', () => {
+    expect(safeTrim(0)).toEqual('0')
+  })
+
+  it('function', () => {
+    let ret = safeTrim(safeTrim(function(){})).replace(/\s+/, '')
+    expect(ret).toEqual('function(){}')
+  })
+})
