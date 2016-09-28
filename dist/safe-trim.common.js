e29d00b085d64920e332abf4b3f8377a0a09bf8b
haozime <a@div.li>
2016-09-28 15:49:03 +0800

    update devDependencies

--- dist/safe-trim.common.js
+++ dist/safe-trim.common.js
@@ -1,5 +1,5 @@
 /*!
- * safe-trim v1.0.6
+ * safe-trim v1.0.9
  * (c) 2016 Jerry
  * Released under the MIT License.
  */
@@ -13,8 +13,8 @@ function safeTrim(string) {
   var CR_LF = '\r\n'; // Used as a new line character in Windows
   var ZERO_WIDTH_SPACE = '\v' + // \x0B VT 垂直制表符
   '\f' + //  \x0C FF 换页符
-  '​‌‍‎‏\u000b\u2028\u2029﻿';
-  var OTHER_SPACE = '  ᠎             　';
+  '\u200B\u200C\u200D\u200E\u200F\x0B\u2028\u2029\uFEFF';
+  var OTHER_SPACE = '\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000';
 
   var ALL_SPACE = SP + TAB + CR + LF + CR_LF + ZERO_WIDTH_SPACE + OTHER_SPACE;
 
