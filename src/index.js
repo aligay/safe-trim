938178a327d31617b501d195368842f958bd9ce6
haozi <syntaxright@gmail.com>
2024-09-26 11:50:08 +0800

    feat: 🎸 Add new zero-width word

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ export default function safeTrim (string) {
                            '\f' + //  \x0C FF 换页符
                            '\u200B\u200C\u200D\u200E\u200F\u000b\u2028\u2029\uFEFF\u202D'
   const OTHER_SPACE =
-    '\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000⁧'
+    '\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000⁧‍'
 
   const ALL_SPACE = SP + TAB + CR + LF + CR_LF + ZERO_WIDTH_SPACE + OTHER_SPACE
 
